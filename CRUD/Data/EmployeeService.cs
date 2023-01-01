9472aaa3cc237e64287d99de8e9fe60073ba2538
Kevin Pandya <kevinpandya02@gmail.com>
2023-01-01 13:10:14 +0530

    Add project files.

---
+++ CRUD/Data/EmployeeService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUD.Data
+{
+    public class EmployeeService : IEmployeeService
+    {
+        private readonly List<Employee> employees = new List<Employee>
+        {
+            new Employee
+            {
+                Id=Guid.NewGuid(),
+                Name = "Employee 1"
+            },
+            new Employee
+            {
+                Id=Guid.NewGuid(),
+                Name = "Employee 2"
+            }
+        };
+
+        public Employee GetEmployee(Guid id)
+        {
+            return employees.SingleOrDefault(x => x.Id == id);
+        }
+
+        public List<Employee> GetEmployees()
+        {
+            return employees;
+        }
+
+        public void UpdateEmployee(Employee employee)
+        {
+            var getOldEmployee = GetEmployee(employee.Id);
+            getOldEmployee.Name = employee.Name;
+        }
+    }
+}
---
+++ CRUD/Data/IEmployeeService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+namespace CRUD.Data
+{
+    interface IEmployeeService
+    {
+        List<Employee> GetEmployees();
+
+        Employee GetEmployee(Guid id);
+
+        void UpdateEmployee(Employee employee);
+    }
+}
